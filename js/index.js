d7907e2705140ecb788bb7610d1b42bd9b7fbf73
s <s@salva.io>
2017-02-14 01:06:57 -0300

    restrict 2 ar

--- js/index.js
+++ js/index.js
@@ -16,7 +16,8 @@ function initAutocomplete() {
   // location types.
   autocomplete = new google.maps.places.Autocomplete(
       /** @type {!HTMLInputElement} */(document.getElementById('autocomplete')),
-      {types: ['geocode']});
+      {types: ['geocode'],
+      componentRestrictions: {country: 'ar'}});
 
   // When the user selects an address from the dropdown, populate the address
   // fields in the form.
